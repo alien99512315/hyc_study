42086bf7c9fb8b97248b3dca89a24203b5c1371a
alien99512315 <18629577976@163.com>
2026-08-08 17:58:21 +0800

    add work

---
+++ build/CMakeFiles/bbb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bbb.dir/src/aaa.cpp.o"
+  "CMakeFiles/bbb.dir/src/aaa.cpp.o.d"
+  "bbb"
+  "bbb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bbb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/aaa.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"请输入a的值";
+    cin>>a;
+    int b;
+    cout<<"请输入b的值";
+    cin>>b;
+    const int c = 6;
+    int d=a+b*c;
+    int e=(a+b)*c;
+    cout<<d<<endl;
+    cout<<e<<endl;
+
+
+
+    return 0;
+}
